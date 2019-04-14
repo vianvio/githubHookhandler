82c2ba140da3c3fc73e220fc647e66de632642d0
磐冲 <lijie.slj@alibaba-inc.com>
2019-04-15 00:11:01 +0800

    fix typo

--- router.py
+++ router.py
@@ -3,7 +3,7 @@ import json
 
 
 def get_route_config(routes):
-    @routes.post('/applicationName')
+    @routes.post('/{applicationName}')
     async def hello(request):
         print(json.dump(request))
         return web.Response(text="received, thank you")

365922241b9db5e5e689d4c4e7cdb04949b0a50b
磐冲 <lijie.slj@alibaba-inc.com>
2019-04-15 14:27:19 +0800

    fix

--- util.py
+++ util.py
@@ -3,4 +3,4 @@ import json
 
 def get_config():
     with open('./config.json', 'r', encoding='utf-8') as config_file:
-        return json.load(config_file.read())
+        return json.load(config_file)

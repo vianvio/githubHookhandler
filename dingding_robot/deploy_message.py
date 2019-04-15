1cd30bd3223054e2a926a0fd8a9098051f98df10
磐冲 <lijie.slj@alibaba-inc.com>
2019-04-15 14:19:28 +0800

    test robot 2

--- dingding_robot/deploy_message.py
+++ dingding_robot/deploy_message.py
@@ -2,22 +2,22 @@ import aiohttp
 import json
 from util import get_config
 
+_clientSession = aiohttp.ClientSession()
+
 
 def send_start_deploy_msg(project_name):
-    async with aiohttp.ClientSession() as session:
-        session.post(
-            get_config()['robotUrl'],
-            data=json.dump({
-                'msgtype': 'text',
-                'text': f'{project_name}开始部署'
-            }))
+    _clientSession.post(
+        get_config()['robotUrl'],
+        data=json.dump({
+            'msgtype': 'text',
+            'text': f'{project_name}开始部署'
+        }))
 
 
 def send_finish_deploy_msg(project_name):
-    async with aiohttp.ClientSession() as session:
-        session.post(
-            get_config()['robotUrl'],
-            data=json.dump({
-                'msgtype': 'text',
-                'text': f'{project_name}部署完毕'
-            }))
+    _clientSession.post(
+        get_config()['robotUrl'],
+        data=json.dump({
+            'msgtype': 'text',
+            'text': f'{project_name}部署完毕'
+        }))
